3641dac44254457a2fd71675c6c377d6cf8775e2
WolfLink115 <wolflink747@gmail.com>
2023-10-15 01:46:50 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # based on the instructions from edk2-platform
 set -e
 . build_common.sh
-
+mkdir -p workspace
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p PixelXLPkg/PixelXLPkg.dsc
 gzip -c < workspace/Build/PixelXLPkg/DEBUG_GCC5/FV/PIXELXLPKG_UEFI.fd >uefi_image
